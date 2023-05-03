35e7ba25c238049574de498c1e1257b07d0981fa
Dion Scott <dionanthonyscott@gmail.com>
2023-05-03 02:25:40 -0500

    Create operate function to switch between math equations

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ function mult(a,b) {
 function div(a,b) {
     return a / b;
 }
+
+function operate(operator, first, second) {
+    switch (operator) {
+        case "+":
+            return add(first, second)
+        case "-":
+            return sub(first, second)
+        case "*":
+            return mult(first, second)
+        case "/":
+            return div(first, second)
+    }
+}
