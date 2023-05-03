874be555937ef84cf77a89c5aae87d5a2ab325e7
Dion Scott <dionanthonyscott@gmail.com>
2023-05-03 03:01:09 -0500

    Add write function to the numbers

--- script.js
+++ script.js
@@ -26,8 +26,49 @@ function operate(operator, first, second) {
             return div(first, second)
     }
 }
+//create the complete input variable
+let completeInput = "";
+function write(input) {
+    //takes an input and displays it
+    //keeps display until an operator, clear, or equal is selected
+    completeInput += input;
+    changeDisplay(completeInput);
+}
 
 function changeDisplay(num) {
     let display = document.querySelector('#display')
     display.textContent = num;
 }
+
+//adding event listeners to numbers
+document.querySelector('#num0').addEventListener('click', function() {
+    write("0");
+});
+document.querySelector('#num1').addEventListener('click', function() {
+    write("1");
+});
+document.querySelector('#num2').addEventListener('click', function() {
+    write("2");
+});
+document.querySelector('#num3').addEventListener('click', function() {
+    write("3");
+});
+document.querySelector('#num4').addEventListener('click', function() {
+    write("4");
+});
+document.querySelector('#num5').addEventListener('click', function() {
+    write("5");
+});
+document.querySelector('#num6').addEventListener('click', function() {
+    write("6");
+});
+document.querySelector('#num7').addEventListener('click', function() {
+    write("7");
+});
+document.querySelector('#num8').addEventListener('click', function() {
+    write("8");
+});
+document.querySelector('#num9').addEventListener('click', function() {
+    write("9");
+});
+
